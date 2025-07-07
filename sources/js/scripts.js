816b39069d7cac2a219c4cb3b5fb15ef5490b166
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-07-07 13:31:30 -0300

    animação nos social icons

--- sources/js/scripts.js
+++ sources/js/scripts.js
@@ -15,8 +15,35 @@ window.onscroll = () => {
 };
 
 
-
 // Animação de carregamento da página
-window.addEventListener('DOMContentLoaded', function() {
-        document.body.classList.add('loaded');
-    });
+window.addEventListener('DOMContentLoaded', function () {
+    document.body.classList.add('loaded');
+});
+
+
+
+// Hover dos social-icons
+document.querySelectorAll(".social-icon").forEach((img) => {
+    const originalImg = img.src;
+    let hoverImg = originalImg;
+
+    if (img.classList.contains("git-icon")) {
+        hoverImg = "/sources/icons/social/Git-rosa.svg";
+    }
+
+    if (img.classList.contains("linkedin-icon")) {
+        hoverImg = "/sources/icons/social/linkedin-rosa.svg";
+    }
+
+    if (img.classList.contains("zap-icon")) {
+        hoverImg = "/sources/icons/social/zap-rosa.svg";
+    }
+
+    img.addEventListener('mouseenter', function () {
+        img.src = hoverImg;
+    })
+
+    img.addEventListener('mouseleave', function () {
+        img.src = originalImg;
+    })
+})
