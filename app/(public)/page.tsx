aefdf149d670a6249e177a95028def62f258d54f
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-09-03 15:58:55 -0300

    moved componets folder

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,5 +1,5 @@
-import HeroPage from '../components/Hero/HeroPage';
-import AboutPage from '../components/About/AboutPage';
+import HeroPage from './components/Hero/HeroPage';
+import AboutPage from './components/About/AboutPage';
 
 export default function Home() {
   return (
