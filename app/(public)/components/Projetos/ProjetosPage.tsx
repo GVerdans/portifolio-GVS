671753895ba18a6297387c33c0409c9159b0cb7a
Gabriel Verdan <tahoriemverdan@gmail.com>
2026-09-03 18:34:07 -0300

    ajustes no header projetos-page

--- app/(public)/components/Projetos/ProjetosPage.tsx
+++ app/(public)/components/Projetos/ProjetosPage.tsx
@@ -4,8 +4,8 @@ import CardsProjetos from '../CardsProjetos/CardsProjetos';
 export default function ProjetosPage() {
   return (
     <PageSection backgroundClassName="bg-(--quartiary)">
-      <div className="flex flex-col w-full">
-        <header className="flex flex-col gap-5 border-b border-(--secondary)/40 pb-8 sm:flex-row sm:items-end sm:justify-between">
+      <div className="flex flex-col w-full h-full">
+        <header className="flex flex-col gap-5 border-b border-(--primary)/40 pb-8 sm:flex-row sm:items-end sm:justify-between">
           <div>
             <p className="mb-3 text-sm uppercase tracking-[0.3em] text-(--secondary)">
               03 /
@@ -15,7 +15,7 @@ export default function ProjetosPage() {
             </h2>
           </div>
         </header>
-        <div className="projetos flex flex-col border border-red-500">
+        <div className="projetos flex flex-row flex-wrap gap-10 mt-10">
           <CardsProjetos
             title="Teste"
             text="Teste"
